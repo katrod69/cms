3d74161ca0d5a7f343d58246228a305ce2351d3a
katrod <katrod4545@gmail.com>
2023-06-23 16:43:15 -0600

    fixed seeded data to diplay

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,29 @@
-USE employee_db;
-INSERT INTO department(id, name) VALUES
-(1)
-(2)
-(3)
-(4)
-(5)
 
-
+INSERT INTO department(name) VALUES
+(C-suite,),
+(Associates,),
+(Janitors,);
 
 INSERT INTO roles(title, salary, department_id) VALUES
-
+('CFO', 100000000, 1),
+('CEO', 200000000, 1),
+('COO', 120000000, 1),
+('ASSOCIATE 1' 300, 2),
+('ASSOCIATE 2' 400, 2),
+('ASSOCIATE 3' 500, 2),
+('JANITOR 1' 500, 3),
+('JANITOR 1' 500, 3),
+('JANITOR 1' 500, 3);
 
 INSERT INTO employees(first_name, last_name, role_id, manager_id) VALUES
+('stinky', 'Maury', 1, NULL),
+('Delorian', 'Timmy', 2, NULL),
+('Fawcet', 'Farra', 3, NULL),
+('Mouse', 'Mickey', 4, 6),
+('Wilkerson', 'Malcolm', 1, NULL),
+('mary', 'watson', 5, 8),
+('nikko', 'Horace', 6, NULL),
+('beans', 'mgee', 7, NULL),
+('petey', 'Bond', 8, NULL),
+('mark', 'toulouse', 9, 4);
+
