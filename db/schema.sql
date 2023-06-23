0562e0131cf2f71374d7310feaed0d327fbe7e15
katrod <katrod4545@gmail.com>
2023-06-23 16:43:05 -0600

    fixed syntax errors

--- db/schema.sql
+++ db/schema.sql
@@ -5,26 +5,26 @@ USE employee_db;
 
 CREATE TABLE department(
     id iNT NOT NULL AUTO_INCREMENT,
-    name varchar(30),
+    name varchar(30) NOT NULL,
     primary key (id)
 );
 
-CREATE TABLE roles(
-    id INT NOT NULL ,
-    title varchar (30),
-    salary decimal,
-    department_id int,
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title varchar (30) NOT NULL,
+    salary decimal (10,2) NOT NULL,
+    department_id int NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee(
-   id INT NOT NULL ,
-   first_name varchar (30),
-   last_name varchar (30),
-   role_id INT,
-   manager_id INT,
-   FOREIGN KEY (role_id) REFERENCES roles(id),
+   id INT NOT NULL AUTO_INCREMENT ,
+   first_name varchar (30) NOT NULL,
+   last_name varchar (30) NOT NULL,
+   role_id INT NOT NULL,
+   manager_id INT NOT NULL,
+   FOREIGN KEY (role_id) REFERENCES role(id),
    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
